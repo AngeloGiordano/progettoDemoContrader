af173305f7d76fcc6d092e4864f80686e663ddda
AngeloGiordano <giordano_angelo@outlook.it>
2018-01-30 16:38:08 +0100

    reg test ok

--- src/main/java/com/example/demo/Controller/LoginController.java
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -1,23 +1,32 @@
 package com.example.demo.Controller;
 
+
+import com.example.demo.Converter.UserConverter;
+import com.example.demo.Dto.UserDTO;
+import com.example.demo.GenericResponse;
 import com.example.demo.Model.User;
 import com.example.demo.Service.LoginService;
+import com.example.demo.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
-@ControllerAdvice
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin(value="*")
+@RestController
 @RequestMapping("/login")
 public class LoginController {
 
     private LoginService loginService;
+    private UserConverter userConverter;
+    private UserService userService;
 
     @Autowired
-    public LoginController(LoginService loginService) {
+    public LoginController(LoginService loginService,UserService userService,UserConverter userConverter)
+    {
         this.loginService = loginService;
+        this.userConverter=userConverter;
+        this.userService=userService;
     }
 
     @RequestMapping(value="/page")
@@ -26,49 +35,43 @@ public class LoginController {
         return "loginPage";
     }
 
-    @RequestMapping(value = "/menu", method = RequestMethod.POST)
-    public String menu (@RequestParam("username") String username, @RequestParam("password") String password, Model model)
+    @RequestMapping(value="/menu", method = RequestMethod.POST)
+    public GenericResponse<UserDTO> menu (@RequestBody UserDTO userDTO)
     {
-        User user = loginService.login(username, password);
+        User u=userConverter.convertToEntity(userDTO);
+        User user = loginService.login(u.getUsername(),u.getPassword());
 
         if (user == null)
-        {
-            model.addAttribute("msg","Credenziali non valide");
-            return "loginPage";
-        }
+            return new GenericResponse<>(1, null);
 
-        model.addAttribute("Nome", user.getNomeUser());
-        model.addAttribute("Cognome", user.getCognomeUser());
+
+        UserDTO duser=userConverter.convertToDTO(user);
 
         if(user.getRuolo().equalsIgnoreCase("admin"))
-            return "menuAdmin";
+            return new GenericResponse<>(2,duser);//per il menuAdmin
         else
-            return "menuUser";
+            return new GenericResponse<>(3,duser);//per il menuUser
     }
 
-    @RequestMapping(value="/regControl", method=RequestMethod.POST)
-    public String control (@ModelAttribute User User, Model model, RedirectAttributes redirectAttrs)
+
+    @RequestMapping(value="/regControl", method = RequestMethod.POST)
+    public GenericResponse<UserDTO> control (@RequestBody UserDTO userDTO)
     {
-        String username = User.getUsername();
-        String password = User.getPassword();
-        User user = loginService.login(username, password);
-        //model.addAttribute("user",user);
-        if (user != null)
-        {
-            model.addAttribute("msg","Username già in uso...");
-            return "insertUser";
-        }
+        User user=userConverter.convertToEntity(userDTO);
+        String username = user.getUsername();
+        String password = user.getPassword();
+        User u = loginService.login(username, password);
+
+        if (u != null)
+            return new GenericResponse<>(0,null);//username già in uso
         else
         {
-            model.addAttribute("user",User);
-            redirectAttrs.addFlashAttribute("user",User);
-            return  "redirect:/users/newUser";
+            User userEntity = userService.insert(user);
 
+            UserDTO duser=userConverter.convertToDTO(user);
+
+            return new GenericResponse<>(1,duser); //inserisce nuovo utente
         }
-
     }
-
-
-
 
 }
